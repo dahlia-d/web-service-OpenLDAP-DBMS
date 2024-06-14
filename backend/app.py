c322b8ce1965e39c8cea3a7b69c6c4901f8f9671
dahlia-d <dahlia.datseva@gmail.com>
2024-06-14 23:53:42 +0300

    frontend

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,71 @@
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask, request, jsonify, make_response
+from sqlalchemy import create_engine, select, Text
+from sqlalchemy.orm import Session, DeclarativeBase, Mapped, mapped_column, session
+import requests
 from flask_cors import CORS
+
 app = Flask(__name__)
 CORS(app)
 
+def engine_create():
+    return create_engine('mariadb+pymysql://user:password@localhost:3306/database')
+
+class Base(DeclarativeBase):
+    pass
+
+class Event(Base):
+    __tablename__ = "event"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[str] = mapped_column(Text)
+    event: Mapped[str] = mapped_column(Text)
+    date: Mapped[str] = mapped_column(Text)
+
+    def __init__(self, user_id, event, date):
+        self.user_id = user_id
+        self.event = event
+        self.date = date
+
+@app.route('/add', methods=['POST'])
+def add_event():
+    data = request.json
+
+    response = requests.post(f"http://localhost:8080/realms/realm/protocol/openid-connect/token/introspect", {"client_id": "backend", "client_secret": "XxDEzLTNNOvsWlZoMIqDGw02wO3GeDdn", "token": data["token"]}).json()
+
+    if response["active"] == False:
+       return jsonify({"message": "Unauthorized user"}), 401
+
+    engine = engine_create()
+
+    user_id = response["sub"]
+    event = data.get('user_event')
+    date = data.get('date')
+
+    new_event = Event(user_id=user_id, event=event, date=date)
+
+    with Session(engine) as session:
+        session.add(new_event)
+        session.commit()
+
+    return make_response({"message": f"Event was added successfully"}, 200)
 
 @app.route('/data', methods=['GET'])
-def get_data():
-    data = {
-        "message": "Hello from Flask!"
-    }
-    return jsonify(data)
+def data():
+    return jsonify({"message": "Hello from app.py"})
+
+@app.route('/events', methods=['POST'])
+def get_events():
+    data = request.json
+    response = requests.post(f"http://localhost:8080/realms/realm/protocol/openid-connect/token/introspect", {"client_id": "backend", "client_secret": "XxDEzLTNNOvsWlZoMIqDGw02wO3GeDdn", "token": data["token"]}).json()
+
+    events = select(Event)
+    events_list = []
+    with Session(engine_create()) as session:
+        for event in session.execute(events):
+            if(event.Event.user_id == response['sub']):
+                events_list.append({"id": event.Event.id, "user_id": event.Event.user_id, "event": event.Event.event, "date": event.Event.date})
+        return jsonify(events_list), 200
+
+#Base.metadata.create_all(engine_create())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,63 +1,98 @@
-import './App.css';
-import React, { useEffect, useState } from 'react';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
-import Keycloak from 'keycloak-js';
-/*
+import Keycloak from "keycloak-js";
+
 let initOptions = {
-  url: 'http://localhost:8080/',
-  realm: 'realm',
-  clientId: 'frontend',
-}
+  url: "http://localhost:8080/",
+  realm: "realm",
+  clientId: "frontend",
+};
 
 let kc = new Keycloak(initOptions);
 
 kc.init({
-  onLoad: 'login-required', // Supported values: 'check-sso' , 'login-required'
-  pkceMethod: 'S256'
-}).then((auth) => {
-  if (!auth) {
-    //window.location.reload();
-  } else {
-    // Remove below logs if you are using this on production
-    console.info("Authenticated");
-    console.log('auth', auth)
-    console.log('Keycloak', kc)
-    console.log('Access Token', kc.token)
-
-    kc.onTokenExpired = () => {
-      console.log('token expired')
-    }
-  }
-}, () => {
-  //Notify the user if necessary
-  console.error("Authentication Failed");
+  onLoad: "login-required", // Supported values: 'check-sso' , 'login-required'
 });
-*/
-function App() {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
 
-  const fetchData = () => {
-    fetch('http://localhost:5000/data')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => setData(data.message))
-      .catch(error => setError(error.toString()));
+function App() {
+  const [message, setMessage] = useState("Befoe adding event");
+  const [user_event, setEvent] = useState("");
+  const [date, setDate] = useState("");
+  const [events, setEvents] = useState([]);
+  
+  const fetchEvents = async (event) => {
+    event.preventDefault();
+      const token = kc.token;
+      console.log(token);
+      const response = await fetch('http://localhost:5000/events', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({token}),
+      });
+      const data = await response.json();
+      setEvents(data);
+  };
+
+  const handleSubmit = async () => {
+    const token = kc.token;
+      const json_body = JSON.stringify({
+        token, user_event, date,
+      });
+      const response = await fetch("http://localhost:5000/add", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: json_body,
+      });
+      const data = await response.json();
   };
 
   return (
-    <div>
-      <header>
-        <h1>{data ? data : "Click the button to load data"}</h1>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        <button onClick={fetchData}>Fetch Data</button>
-      </header>
+    <div className="App">
+      <h1>Add event</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Event:
+            <input
+              type="event"
+              value={user_event}
+              onChange={(e) => setEvent(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Date:
+            <input
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">Add event</button>
+      </form>
+      <h1>Show events</h1>
+      <button onClick={fetchEvents}>Show events</button>
+      <ul>
+        {events.map(event => (
+          <li key={event.id}>
+            <p>Event: {event.event}</p>
+            <p>Date: {event.date}</p>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => {kc.logout()}}>Log out</button>
     </div>
   );
 }
 
+
 export default App;
+
+
